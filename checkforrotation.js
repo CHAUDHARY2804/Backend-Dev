b8633168fc81feca001ef2bcb0ae712ac3760153
CHAUDHARY2804 <nc6168358@gmail.com>
2026-01-15 14:59:52 +0530

    second commit

--- checkforrotation.js
+++
@@ -1,6 +0,0 @@
-// Check for Rotation Given two strings, check if one is a rotation of another. Input: "ABCD", "CDAB" → Output: true
-
-let str1 = "ABCD"
-let str2 = "ADCB"
-let isRotation = (str1.length === str2.length) && (str1 + str1).includes(str2)
-console.log(isRotation)
