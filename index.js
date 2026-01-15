0be95b2aa663eb358bbb1ad422a7a056151387b7
CHAUDHARY2804 <nc6168358@gmail.com>
2026-01-15 14:57:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//1 hello world
+console.log("Node.js Backend is started");
+
+//2 variables
+let name="Aman";  //strings
+let age=22;        //number
+const country ="India"; //constant
+let isStudent = true;   //boolean
+
+console.log(name);
+console.log(age);   
+console.log(country);
+console.log(isStudent);
