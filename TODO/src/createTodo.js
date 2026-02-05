322af17cf64367b8c7b9454df3023cbd3aaf9d17
CHAUDHARY2804 <nc6168358@gmail.com>
2026-02-05 10:52:51 +0530

    [Nikhil].add add the Todo

---
+++ TODO/src/createTodo.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+function createTodo(name, task, status) {
+    try{
+        let ob= {
+            id: new Date(),
+            createtime: new Date(),
+            task, status: false,
+        }
+        if(fs.existsSync("todo.json")){
+            let data = JSON.parse(fs.readFileSync("todo.json","utf-8"));
+            for(let i=0;i<data.length;i++){
+                if(data[i].name === name){
+                    data[i].todo.push(ob);
+                    // delete data[i].todo.pop();
+                }
+            }
+            fs.writeFileSync("todo.json", JSON.stringify(data,null));
+            console.log("todo created");
+        }
+    }
+    catch(err){
+        console.log("error");
+    }
+}
+export default createTodo;
+
+            
+           
---
+++ TODO/src/index.js
@@ -0,0 +1,10 @@
+// import registerUser from "./src/registerUser.js";
+
+// console.log(registerUser("Nikhil","nc6168358@gmail.com","nikhil123"));
+// import loginUser from "./src/loginuser.js";
+// console.log(loginUser("nc6168358@gmail.com","nikhil123"));        
+// import createTodo from "./src/createTodo.js";
+// createTodo("Nikhil","learn fs module");    
+
+import del from "./del.js";
+del("Nikhil");
